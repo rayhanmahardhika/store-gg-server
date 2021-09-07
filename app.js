7e2bebe58314887d03d858d93535a37e173f1916
rayhanmahardhika <18523055@students.uii.ac.id>
2021-09-07 11:36:43 +0700

    init admin lte + reconstructing views + navbar

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/adminlte', express.static(path.join(__dirname, '/node_modules/admin-lte')))
 
 // penggunaan router
 app.use('/', categoryRouter);
