7829453d4cc69935a2154f00c69105bbb9fd5757
Rayhan Mahardhika Wijaya <rayhanmahardhika2000@gmail.com>
2021-10-03 23:50:40 +0700

    fix v2 dashboard API res

--- app/player/controller.js
+++ app/player/controller.js
@@ -212,7 +212,7 @@ module.exports = {
         .populate("category")
         .sort({ updatedAt: -1 });
 
-      res.status(200).json({ data: { history, count } });
+      res.status(200).json({ data: [history, count] });
     } catch (err) {
       res.status(500).json({ message: err.message || "Internal Server Error" });
     }
